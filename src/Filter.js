f1767fe336ac148f51a18ee2fa1715c245063e1b
Emilio Perez <emiperez@fiestas.net>
2021-03-11 18:50:29 +0100

    fetch max questions on level change.

--- src/Filter.js
+++ src/Filter.js
@@ -3,42 +3,49 @@ import LevelSelect from "./LevelSelect";
 import config from "./config.json";
 
 export default class Filter extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			level: this.props.level,
-			maxQuestions: 6,
-			questionAmount: parseInt(this.props.questionAmount)
-		};
+    this.state = {
+      level: this.props.level,
+      maxQuestions: 6,
+      questionAmount: parseInt(this.props.questionAmount)
+    };
 
-		this.handleLevelChange = this.handleLevelChange.bind(this);
-		this.handleChangeQuestionAmount = this.handleChangeQuestionAmount.bind(
-			this
-		);
-	}
+    this.handleLevelChange = this.handleLevelChange.bind(this);
+    this.handleChangeQuestionAmount = this.handleChangeQuestionAmount.bind(
+      this
+    );
+  }
 
-	handleLevelChange() { }
+  handleLevelChange(e) {
+    fetch(config.apiUrl + "/translations/count/es/de/" + e.target.value)
+      .then(response => response.text())
+      .then(data => this.setState({ maxQuestions: parseInt(data) }));
+  }
 
-	handleChangeQuestionAmount() {
-		this.setState({
-			questionAmount: this.state.questionAmount + 1
-		});
-	}
+  handleChangeQuestionAmount(e) {
+    this.setState({
+      questionAmount: e.target.value
+    });
+  }
 
-	render() {
-		return (
-			<>
-				<LevelSelect
-					onChange={this.handleLevelChange}
-					value={this.state.level}/>
-				<input
-					id="questionAmount"
-					type="range"
-					max={this.state.maxQuestions}
-					onChange={this.handleChangeQuestionAmount}/>
-				<span id="questionAmount">{this.state.questionAmount}</span>
-			</>
-		);
-	}
+  render() {
+    return (
+      <>
+        <LevelSelect
+          onChange={this.handleLevelChange}
+          value={this.state.level}
+        />
+        <input
+          id="questionAmount"
+          type="range"
+          max={this.state.maxQuestions}
+          onChange={this.handleChangeQuestionAmount}
+        />
+        <span id="questionAmount">{this.state.questionAmount}</span>
+        <div>{this.state.maxQuestions}</div>
+      </>
+    );
+  }
 }
