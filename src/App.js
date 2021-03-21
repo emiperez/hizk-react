225734f729feaa9781fc219a5f9336e6e6889425
Emilio Perez <emiperez@fiestas.net>
2021-03-21 18:31:03 +0100

    Routing

--- src/App.js
+++ src/App.js
@@ -1,96 +1,45 @@
 import React from "react";
-import QuestionFilter from "./QuestionFilter";
-import Translation from "./Translation";
-import config from "./config.json";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Exam from "./Exam";
 import "./style.css";
 
-export default class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.answers = new Map();
-		this.state = { exam: null, corrections: null };
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Exam</Link>
+            </li>
+            <li>
+              <Link to="/record">Record</Link>
+            </li>
+          </ul>
+        </nav>
 
-		this.handleStartExam = this.handleStartExam.bind(this);
-		this.handleChangeTranslation = this.handleChangeTranslation.bind(this);
-		this.handleGradeExam = this.handleGradeExam.bind(this);
-	}
-
-	handleStartExam(exam) {
-		this.answers = new Map(exam.questions.map(q => [q.id, {id: q.id, locale: exam.answerLocale, text: ""}]));
-		this.setState({ exam: exam, corrections: null }, this.resetAnswerInputs());
-	}
-
-	handleChangeTranslation(e, questionId) {
-		this.answers.set(questionId, { id: questionId, locale: this.state.exam.answerLocale, text: e.target.value });
-	}
-
-	handleGradeExam() {
-		let strAnswers = JSON.stringify(Array.from(this.answers.values()));
-		fetch(config.apiUrl + "/exams/answers/" + this.state.exam.id,
-			{
-				headers: { 'Content-Type': 'application/json' },
-				method: "PUT", body: strAnswers
-			})
-			.then(response => response.json())
-			.then(data => {
-				const correctionMap = new Map(data.map(d => [d.id, d]));
-				this.setState(
-					{ corrections: correctionMap },
-					console.log("STATE CORRECTIONS: " + JSON.stringify(Array.from(correctionMap.entries())) + "/ " + strAnswers)
-				);
-
-			});
-	}
-
-	showCorrection(id) {
-		if (this.state.corrections && this.state.corrections.get(id)) {
-			console.log("showCorrection: " + this.state.corrections.get(id).text);
-			return this.state.corrections.get(id).text;
-		}
-		return null;
-	}
-
-	render() {
-		return (
-			<>
-				<div>
-					<QuestionFilter
-						level="B1"
-						questionLocale="es"
-						answerLocale="de"
-						caseSensitive={true}
-						questionAmount={15}
-						maxQuestions={15}
-						latest={100}
-						onStartExam={this.handleStartExam} />
-				</div>
-				{this.state.exam &&
-					<div id="questionForm">
-						{this.state.exam.questions.map(question => (
-							<div key={question.id} className="examQuestion">
-								<Translation
-									id={question.id}
-									origin={question.text}
-									originLocale={question.locale}
-									target=""
-									targetLocale={this.state.exam.answerLocale}
-									mode="exam"
-									onChange={e => this.handleChangeTranslation(e, question.id)} />
-								{this.showCorrection(question.id)}
-							</div>
-						))}
-						<div>
-							<button onClick={this.handleGradeExam}>Grade Exam</button>
-						</div>
-					</div>
-				}
-			</>
-		);
-	}
-
-	resetAnswerInputs() {
-		Array.from(document.getElementsByClassName("guessTerm")).forEach((el) => {
-			el.value = "";
-		});
-	}
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/record">
+            <Record />
+          </Route>
+          <Route path="/">
+            <Exam />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
+
+function Record() {
+  return <h2>Record</h2>;
+}
+
---
+++ src/Exam.js
@@ -0,0 +1,99 @@
+import React from "react";
+import QuestionFilter from "./QuestionFilter";
+import Translation from "./Translation";
+import config from "./config.json";
+
+export default class Exam extends React.Component {
+	constructor(props) {
+		super(props);
+		this.answers = new Map();
+		this.state = { exam: null, corrections: null };
+
+		this.handleStartExam = this.handleStartExam.bind(this);
+		this.handleChangeTranslation = this.handleChangeTranslation.bind(this);
+		this.handleGradeExam = this.handleGradeExam.bind(this);
+	}
+
+	handleStartExam(exam) {
+		this.answers = new Map(exam.questions.map(q => [q.id, { id: q.id, locale: exam.answerLocale, text: "" }]));
+		this.setState({ exam: exam, corrections: null }, this.resetAnswerInputs());
+	}
+
+	handleChangeTranslation(e, questionId) {
+		this.answers.set(questionId, { id: questionId, locale: this.state.exam.answerLocale, text: e.target.value });
+	}
+
+	handleGradeExam() {
+		let strAnswers = JSON.stringify(Array.from(this.answers.values()));
+		fetch(config.apiUrl + "/exams/answers/" + this.state.exam.id,
+			{
+				headers: { 'Content-Type': 'application/json' },
+				method: "PUT", body: strAnswers
+			})
+			.then(response => response.json())
+			.then(data => {
+				const correctionMap = new Map(data.map(d => [d.id, d]));
+				this.setState(
+					{ corrections: correctionMap },
+					console.log("STATE CORRECTIONS: " + JSON.stringify(Array.from(correctionMap.entries())) + "/ " + strAnswers)
+				);
+
+			});
+	}
+
+	showCorrection(id) {
+		let className = "rightAnswer";
+		if (this.state.corrections && this.state.corrections.get(id)) {
+			console.log("showCorrection: " + this.state.corrections.get(id).text);
+			if (this.state.corrections.get(id).text !== this.answers.get(id).text) {
+				className = "wrongAnswer";
+			}
+			return <span className={className}>{this.state.corrections.get(id).text}</span>;
+		}
+		return null;
+	}
+
+	render() {
+		return (
+			<>
+				<div>
+					<QuestionFilter
+						level="B1"
+						questionLocale="es"
+						answerLocale="de"
+						caseSensitive={true}
+						questionAmount={15}
+						maxQuestions={15}
+						latest={15}
+						onStartExam={this.handleStartExam} />
+				</div>
+				{this.state.exam &&
+					<div id="questionForm">
+						{this.state.exam.questions.map(question => (
+							<div key={question.id} className="examQuestion">
+								<Translation
+									id={question.id}
+									origin={question.text}
+									originLocale={question.locale}
+									target=""
+									targetLocale={this.state.exam.answerLocale}
+									mode="exam"
+									onChange={e => this.handleChangeTranslation(e, question.id)} />
+								{this.showCorrection(question.id)}
+							</div>
+						))}
+						<div>
+							<button onClick={this.handleGradeExam}>Grade Exam</button>
+						</div>
+					</div>
+				}
+			</>
+		);
+	}
+
+	resetAnswerInputs() {
+		Array.from(document.getElementsByClassName("guessTerm")).forEach((el) => {
+			el.value = "";
+		});
+	}
+}
